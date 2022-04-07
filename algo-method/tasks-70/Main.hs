b61fe801a4446a3c751e0da37de6c81fb709c4af
Acasune <tttt4aaaa@gmail.com>
2022-04-07 21:35:37 +0900

    solve:algo-method standard output

---
+++ algo-method/tasks-70/Main.hs
@@ -0,0 +1,7 @@
+module Main where
+
+main :: IO ()
+main = do
+  putStrLn "1"
+  putStrLn "2"
+  putStrLn "3"
---
+++ algo-method/tasks-72/Main.hs
@@ -0,0 +1,5 @@
+module Main where
+
+main :: IO ()
+main = do
+  print $ (*) 314 (159+265) - 358
